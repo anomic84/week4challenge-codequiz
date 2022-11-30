50f9222d4cf0e79f63482e06f1f21ad27559d799
anomic84@gmail.com <spencercarlson@Spencers-MacBook-Pro-2.local>
2022-11-29 21:43:26 -0800

    psuedocode

---
+++ script.js
@@ -0,0 +1,30 @@
+// variables for buttons and containers or windows that will switch
+
+
+// startbutton eventlistener
+
+
+// start quiz function, hides start quiz screen and shows  questions and answers screen
+
+
+
+// start timer
+
+
+// next question funtion
+
+
+
+// show question function that  fills in question from array with array of answers filling in button (appendChild(button))
+
+
+// function to reset 
+
+
+// function to let person know if answer is correct or not, color changes
+
+
+//function to switch to highscores from main menu
+
+
+//function to enter highscores to local
